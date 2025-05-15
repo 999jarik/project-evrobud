fdf323cbd9f785e5e6f5c53d510de0c507622a26
Yaroslav Trofimenko <999jarik@gmail.com>
2025-05-15 17:34:27 +0300

    Added files

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// Налаштування плавності скролу
+SmoothScroll({
+animationTime : 600,
+stepSize : 90,
+accelerationDelta : 40,
+accelerationMax : 2,
+keyboardSupport : true,
+arrowScroll : 50,
+pulseAlgorithm : true,
+pulseScale : 4,
+pulseNormalize : 1,
+touchpadSupport : true,
+})
+
+// Налаштування верхнього слайдеру категорій
+const swiperSlider = new Swiper('.slider-1', {
+    direction: 'horizontal',
+    loop: false,
+    autoplay: {
+      delay: 5000,
+    },
+    grabCursor: true,
+    speed: 1000,
+
+    navigation: {
+      nextEl: '.slide-next',
+      prevEl: '.slide-prev',
+    }
+  });
+// Налаштування другого слайдеру асортименту
+const titleSlides = [];
+document.querySelectorAll('.asort-slide-desc h3').forEach(e => {
+  titleSlides.push(e.innerText);
+})
+
+
+const asortSlider = new Swiper('.slider-2', {
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  effect: 'slide',
+  speed: 1000,
+
+  pagination: {
+    el: '.asort-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<span class="${className}">${titleSlides[index]}</span>`;
+    },
+  }
+});
+
+var player = new Playerjs({
+  id:"player", 
+  file:"../video/test-video.mp4",
+  autoplay: 1
+});
